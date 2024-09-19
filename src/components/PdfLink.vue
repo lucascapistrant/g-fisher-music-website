23ccc4cf9b55974dcb88098eea7fade294a76bd5
Lucas Capistrant <lucas.o.capistrant@gmail.com>
2024-09-18 22:18:08 -0500

    basic pdf links structure

---
+++ src/components/PdfLink.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="pieceScore">
+    <h2>Piece Name</h2>
+    <a :href="pdfPath" target="_blank">See Score</a>
+  </div>
+</template>
+  
+<script>
+  export default {
+    name: "PdfLink",
+    props: {
+      pdfPath: {
+        type: String,
+        required: true,
+      },
+    }
+  }
+</script>
+  
+<style>
+main {
+    width: 100%;
+}
+iframe {
+    width: 500px;
+    height: 1000px;
+}
+</style>
+  
--- src/views/Scores.vue
+++ src/views/Scores.vue
@@ -1,14 +1,34 @@
 <template>
     <div class="page-padding"></div>
-    <h1>Hello world</h1>
-</template>
-
-<script>
-export default {
+    <h1 class="page-header">Scores</h1>
+    <PdfLink v-for="path in pdfPaths" :key="path.id" :pdfPath="path.path" />
+  </template>
+  
+  <script>
+  import PdfLink from '@/components/PdfLink.vue';
+  
+  export default {
     name: 'Scores',
-}
-</script>
-
-<style>
-
-</style>
+    components: {
+      PdfLink
+    },
+    data() {
+      return {
+        pdfPaths: [
+          {
+            id: 1,
+            path: '/pdf/composition_upload(3).pdf'
+          },
+          {
+            id: 2,
+            path: '/pdf/Within The Bazaar!.pdf'
+          },
+        ]
+      };
+    },
+  }
+  </script>
+  
+  <style>
+  </style>
+  
